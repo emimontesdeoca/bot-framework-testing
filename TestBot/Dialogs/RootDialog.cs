e3e6f891f1c557289a7bbf8da7b31a4cdaaadcd7
Emiliano Montesdeca <emontesdeoc@gmail.com>
2018-03-20 12:28:23 +0000

    Nuevas pruebas

--- TestBot/Dialogs/RootDialog.cs
+++ TestBot/Dialogs/RootDialog.cs
@@ -11,41 +11,92 @@ namespace TestBot.Dialogs
     [Serializable]
     public class RootDialog : IDialog<object>
     {
-        public static Dictionary<string, string> data = new Dictionary<string, string>{
+        public static Dictionary<string, string> dataText = new Dictionary<string, string>{
             {"Nuevo", "Que quieres crear?"},
-            {"Ayuda", "Ya te ayudas!"},
+            {"Ayuda", "Ya te ayudamos!"},
             {"Adios", "Nos vemos!"}
+        };
+
+        public static Dictionary<string, Attachment> dataAtt = new Dictionary<string, Attachment>{
+            {
+          "Coche",
+          new Attachment() {
+           ContentUrl = "https://media.ed.edmunds-media.com/subaru/impreza/2006/oem/2006_subaru_impreza_sedan_sti_fq_oem_1_500.jpg",
+            ContentType = "image/png",
+            Name = "Subaru_Impreza.png"
+          }
+         },
+         {
+          "Moto",
+          new Attachment() {
+           ContentUrl = "http://motos.honda.com.co/sites/default/files/motos/cb-1000-r-cc-menu-honda.png",
+            ContentType = "image/png",
+            Name = "moto.png"
+          }
+         },
+         {
+          "Perro",
+          new Attachment() {
+           ContentUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Taka_Shiba.jpg/1200px-Taka_Shiba.jpg",
+            ContentType = "image/png",
+            Name = "ShibaInu.png"
+          }
+         }
         };
 
         public Task StartAsync(IDialogContext context)
         {
-            context.Wait(MessageReceivedAsync);
+            context.Wait(MessageReceivedWithAttachmentAsync);
 
             return Task.CompletedTask;
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        public async Task MessageReceivedWithTextAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            string r = "";
 
-            string text = "";
-
-            foreach (var item in data)
+            foreach (var item in dataText)
             {
                 if (item.Key == activity.Text)
                 {
-                    text = item.Value;
+                    r = item.Value;
                     break;
                 }
             }
 
-            // return our reply to the user
-            await context.PostAsync(text);
+            if (string.IsNullOrWhiteSpace(r) || r == null)
+            {
+                r = "No tengo respuesta para eso.";
+            }
 
-            context.Wait(MessageReceivedAsync);
+            // return our reply to the user
+            await context.PostAsync(r);
+
+            //context.Wait(MessageReceivedWithTextAsync);
+        }
+
+        public async Task MessageReceivedWithAttachmentAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var activity = await result as Activity;
+
+            var r = context.MakeMessage();
+
+            foreach (var item in dataAtt)
+            {
+                if (item.Key == activity.Text)
+                {
+                    r.Attachments.Add(item.Value as Attachment);
+                    break;
+                }
+            }
+
+
+            // return our reply to the user
+            await context.PostAsync(r);
+
+            context.Wait(MessageReceivedWithAttachmentAsync);
         }
     }
 }
--- Tests/Dialogs/RootDialogTest.cs
+++ Tests/Dialogs/RootDialogTest.cs
@@ -5,16 +5,19 @@ using Microsoft.QualityTools.Testing.Fakes;
 using TestBot.Dialogs;
 using Microsoft.Bot.Connector;
 using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs.Fakes;
 
 namespace Blog.Tests.Dialogs
 {
     [TestClass]
     public class RootDialogTests
     {
+        string errorMessage = "No tengo respuesta para eso.";
+
         [TestMethod]
-        public async Task Pregunta_respuesta_prueba()
+        public async Task TestShouldReturnText()
         {
-            foreach (var item in RootDialog.data)
+            foreach (var item in RootDialog.dataText)
             {
                 var preg = item.Key;
                 var resp = item.Value;
@@ -24,6 +27,191 @@ namespace Blog.Tests.Dialogs
                     // Arrange
                     var waitCalled = false;
                     var message = string.Empty;
+
+                    var target = new RootDialog();
+
+                    var activity = new Activity(ActivityTypes.Message)
+                    {
+                        Text = preg
+                    };
+
+                    var awaiter = new Microsoft.Bot.Builder.Internals.Fibers.Fakes.StubIAwaiter<IMessageActivity>()
+                    {
+                        IsCompletedGet = () => true,
+                        GetResult = () => activity
+                    };
+
+                    var awaitable = new Microsoft.Bot.Builder.Dialogs.Fakes.StubIAwaitable<IMessageActivity>()
+                    {
+                        GetAwaiter = () => awaiter
+                    };
+
+                    var context = new Microsoft.Bot.Builder.Dialogs.Fakes.StubIDialogContext();
+
+                    Microsoft.Bot.Builder.Dialogs.Fakes.ShimExtensions.PostAsyncIBotToUserStringStringCancellationToken = (user, s1, s2, token) =>
+                    {
+
+                        message = s1;
+                        return Task.CompletedTask;
+                    };
+
+                    Microsoft.Bot.Builder.Dialogs.Fakes.ShimExtensions.WaitIDialogStackResumeAfterOfIMessageActivity = (stack, callback) =>
+                    {
+                        if (waitCalled) return;
+
+                        waitCalled = true;
+
+                        // The callback is what is being tested.
+                        callback(context, awaitable);
+                    };
+
+                    // Act
+                    //await target.StartAsync(context);
+                    await target.MessageReceivedWithTextAsync(context, awaitable);
+
+                    // Assert
+                    Assert.AreEqual(resp, message);
+                }
+            }
+        }
+
+        [TestMethod]
+        public async Task TestShouldReturnErrorIfEmptyOrSpaces()
+        {
+            List<string> strings = new List<string>() { "", "      ", null };
+
+            foreach (var item in strings)
+            {
+                var preg = item;
+
+                using (ShimsContext.Create())
+                {
+                    // Arrange
+                    var waitCalled = false;
+                    var message = string.Empty;
+
+                    var target = new RootDialog();
+
+                    var activity = new Activity(ActivityTypes.Message)
+                    {
+                        Text = preg
+                    };
+
+                    var awaiter = new Microsoft.Bot.Builder.Internals.Fibers.Fakes.StubIAwaiter<IMessageActivity>()
+                    {
+                        IsCompletedGet = () => true,
+                        GetResult = () => activity
+                    };
+
+                    var awaitable = new Microsoft.Bot.Builder.Dialogs.Fakes.StubIAwaitable<IMessageActivity>()
+                    {
+                        GetAwaiter = () => awaiter
+                    };
+
+                    var context = new Microsoft.Bot.Builder.Dialogs.Fakes.StubIDialogContext();
+
+                    Microsoft.Bot.Builder.Dialogs.Fakes.ShimExtensions.PostAsyncIBotToUserStringStringCancellationToken = (user, s1, s2, token) =>
+                    {
+
+                        message = s1;
+                        return Task.CompletedTask;
+                    };
+
+                    Microsoft.Bot.Builder.Dialogs.Fakes.ShimExtensions.WaitIDialogStackResumeAfterOfIMessageActivity = (stack, callback) =>
+                    {
+                        if (waitCalled) return;
+
+                        waitCalled = true;
+
+                        // The callback is what is being tested.
+                        callback(context, awaitable);
+                    };
+
+                    // Act
+                    //await target.StartAsync(context);
+                    await target.MessageReceivedWithTextAsync(context, awaitable);
+
+                    // Assert
+                    Assert.AreEqual(message, errorMessage);
+                }
+            }
+        }
+
+        [TestMethod]
+        public async Task TestShouldReturnErrorIfTextNotFound()
+        {
+            List<string> strings = new List<string>() { "abcdf", "emi" };
+
+            foreach (var item in strings)
+            {
+                var preg = item;
+
+                using (ShimsContext.Create())
+                {
+                    // Arrange
+                    var waitCalled = false;
+                    var message = string.Empty;
+
+                    var target = new RootDialog();
+
+                    var activity = new Activity(ActivityTypes.Message)
+                    {
+                        Text = preg
+                    };
+
+                    var awaiter = new Microsoft.Bot.Builder.Internals.Fibers.Fakes.StubIAwaiter<IMessageActivity>()
+                    {
+                        IsCompletedGet = () => true,
+                        GetResult = () => activity
+                    };
+
+                    var awaitable = new Microsoft.Bot.Builder.Dialogs.Fakes.StubIAwaitable<IMessageActivity>()
+                    {
+                        GetAwaiter = () => awaiter
+                    };
+
+                    var context = new Microsoft.Bot.Builder.Dialogs.Fakes.StubIDialogContext();
+
+                    Microsoft.Bot.Builder.Dialogs.Fakes.ShimExtensions.PostAsyncIBotToUserStringStringCancellationToken = (user, s1, s2, token) =>
+                    {
+
+                        message = s1;
+                        return Task.CompletedTask;
+                    };
+
+                    Microsoft.Bot.Builder.Dialogs.Fakes.ShimExtensions.WaitIDialogStackResumeAfterOfIMessageActivity = (stack, callback) =>
+                    {
+                        if (waitCalled) return;
+
+                        waitCalled = true;
+
+                        // The callback is what is being tested.
+                        callback(context, awaitable);
+                    };
+
+                    // Act
+                    //await target.StartAsync(context);
+                    await target.MessageReceivedWithTextAsync(context, awaitable);
+
+                    // Assert
+                    Assert.AreEqual(message, errorMessage);
+                }
+            }
+        }
+
+        [TestMethod]
+        public async Task Bot_Test_Attachments()
+        {
+            foreach (var item in RootDialog.dataAtt)
+            {
+                var preg = item.Key;
+                var att = item.Value;
+
+                using (ShimsContext.Create())
+                {
+                    // Arrange
+                    var waitCalled = false;
+                    var message = new object();
 
                     var target = new RootDialog();
 
@@ -62,10 +250,11 @@ namespace Blog.Tests.Dialogs
                     };
 
                     // Act
-                    await target.StartAsync(context);
+                    // await target.StartAsync(context);
+                    await target.MessageReceivedWithAttachmentAsync(context, awaitable);
 
                     // Assert
-                    Assert.AreEqual(resp, message);
+                    Assert.AreEqual(att, message);
                 }
             }
         }
